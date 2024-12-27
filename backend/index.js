91178f43d93be4083b2a25b64a468341131fba82
Muhammad Zubair Ahmad <129215320+mzahmad889889@users.noreply.github.com>
2024-12-27 14:55:05 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://booking-app-frontend-topaz.vercel.app',
+    origin: 'https://booking-app-mern.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
