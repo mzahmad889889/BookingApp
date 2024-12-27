248dc440fd5c97b5e20a1bff3b5594fcd4731c19
Muhammad Zubair Ahmad <129215320+mzahmad889889@users.noreply.github.com>
2024-12-27 20:00:32 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-import axios from 'axios';
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import BookingForm from "./components/BookingForm"
 import ProductList from "./components/ProductList"
 import Checkout from "./components/Checkout"
 
 function App() {
-axios.defaults.withCredentials = true
+
   return (
     <>
       <BrowserRouter>
