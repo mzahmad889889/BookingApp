ac79fd52a9c0862ec22ce2f1b9f344a6cefc4053
Muhammad Zubair Ahmad <129215320+mzahmad889889@users.noreply.github.com>
2024-12-28 14:47:40 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://booking-app-frontend-kappa.vercel.app/', // Remove trailing slash
+    origin: 'https://booking-app-frontend-kappa.vercel.app', // Remove trailing slash
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Ensure headers are explicitly allowed
     credentials: true, // Allow cookies (if needed)
